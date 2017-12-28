68acf3346fefbb0a88b0354c16d6136fc46f0221
Buğra Ekuklu <ekuklu@icloud.com>
2017-12-28 14:30:53 +0300

    Permissions for maintaining access.

---
+++ apps/legion/lib/legion/identity/auth/access_control/permission.ex
@@ -0,0 +1,25 @@
+defmodule Legion.Identity.Auth.AccessControl.Permission do
+  @moduledoc """
+  Permissions.
+  """
+  use Legion.Stereotype, :model
+
+  alias Legion.Identity.Auth.AccessControl.PermissionSet
+  alias Legion.Identity.Auth.AccessControl.ControllerAction
+
+  schema "permissions" do
+    field :controller_name, :string
+    field :controller_action, ControllerAction
+    field :type, :string
+
+    many_to_many :permission_sets,
+                  PermissionSet,
+                  join_through: "permission_set_permissions"
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:controller_name, :controller_action, :type])
+    |> validate_required([:controller_name, :controller_action, :type])
+  end
+end
