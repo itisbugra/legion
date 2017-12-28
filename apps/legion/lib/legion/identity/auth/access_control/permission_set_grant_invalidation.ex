11a4dd7d068a6174a8fb84a84aaac492fc3971f6
Buğra Ekuklu <ekuklu@icloud.com>
2017-12-28 14:32:07 +0300

    Grant invalidations.

---
+++ apps/legion/lib/legion/identity/auth/access_control/permission_set_grant_invalidation.ex
@@ -0,0 +1,24 @@
+defmodule Legion.Identity.Auth.AccessControl.PermissionSetGrant.Invalidation do
+  @moduledoc """
+  Represents an invalidation order of permission set grant.
+  """
+  use Legion.Stereotype, :model
+
+  alias Legion.Identity.Auth.AccessControl.PermissionSetGrant
+  alias Legion.Identity.Information.Registration
+
+  schema "permission_set_grant_invalidations" do
+    belongs_to :grant, PermissionSetGrant
+    belongs_to :authority, Registration
+    field :inserted_at, :naive_datetime, read_after_writes: true
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:grant_id, :authority_id])
+    |> validate_required([:grant_id, :authority_id])
+    |> foreign_key_constraint(:grant_id)
+    |> foreign_key_constraint(:authority_id)
+    |> unique_constraint(:grant_id)
+  end
+end
