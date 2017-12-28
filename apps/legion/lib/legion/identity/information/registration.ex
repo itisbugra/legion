2da672e18c264505cc6f239547391f9f1d7b28e5
Buğra Ekuklu <ekuklu@icloud.com>
2017-12-28 14:31:44 +0300

    Granular model of user registration.

---
+++ apps/legion/lib/legion/identity/information/registration.ex
@@ -0,0 +1,15 @@
+defmodule Legion.Identity.Information.Registration do
+  @moduledoc """
+  Represents registrations of users.
+  """
+  use Legion.Stereotype, :model
+
+  schema "user_registrations" do
+    field :inserted_at, :naive_datetime, read_after_writes: true
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [])
+  end
+end
