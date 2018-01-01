c14a0e3e6d8294a2c27530dfda278d4175ad7b65
Buğra Ekuklu <ekuklu@icloud.com>
2018-01-02 00:44:46 +0300

    Added configurations for new features.

--- apps/legion/config/canary.exs
+++ apps/legion/config/canary.exs
@@ -1,8 +1,16 @@
 use Mix.Config
 
 six_months = 15_552_000
+half_of_an_hour = 3_600
+three_minutes = 180
 
-# Configure access control basics
+config :ua_inspector,
+  database_path: Path.join(Mix.Utils.mix_home, "ua_inspector")
+
+config :freegeoip, 
+  base_url: "http://localhost:8080"
+
+# Access control basics
 config :legion, Legion.Identity.Auth.AccessControl,
   permission_set_name_length: 4..28,
   permission_set_description_length: 8..80,
@@ -10,3 +18,34 @@ config :legion, Legion.Identity.Auth.AccessControl,
   maximum_granting_lifetime: six_months,
   allow_infinite_lifetime: true
 
+# Concrete authentication basics
+config :legion, Legion.Identity.Auth.Concrete,
+  passkey_scaling: 4,
+  passphrase_lifetime: six_months
+
+# JSON Object Signing and Encoding (JOSE) settings
+config :legion, Legion.Identity.Auth.Concrete.JOSE,
+  secret_key_base: "secret_key_base",
+  issuer: "legion",
+  lifetime: half_of_an_hour,
+  sub: "access"
+
+config :legion, Legion.Identity.Auth.Insecure,
+  allowed_cleartext_safe_artifacts: [:username, :email],
+  username_type: :alphanumeric,
+  username_length: 6..20,
+  username_requires_check: false,
+  bypass_concrete_auth: true,
+  password_digestion: :bcrypt,
+  password_type: :unicode,
+  password_length: 8..32,
+  password_minimum_security_level: :all
+
+config :legion, Legion.Identity.Auth.OTC,
+  lifetime: three_minutes,
+  type: :integer,
+  length: 6,
+  enable_zero_padding: false,
+  prefix: "L",
+  postfix: "",
+  medium: Legion.Identity.Auth.OTC.Adapters.SMS
