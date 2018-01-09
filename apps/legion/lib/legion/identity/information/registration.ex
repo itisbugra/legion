1a9975ae71b3c6effbdd8b1055512f8eaa842546
Buğra Ekuklu <ekuklu@icloud.com>
2018-01-09 18:06:47 +0300

    GPS telemetry consent on user.

--- apps/legion/lib/legion/identity/information/registration.ex
+++ apps/legion/lib/legion/identity/information/registration.ex
@@ -5,6 +5,7 @@ defmodule Legion.Identity.Information.Registration do
   use Legion.Stereotype, :model
 
   schema "user_registrations" do
+    field :has_gps_telemetry_consent?, :boolean, default: false
     field :inserted_at, :naive_datetime, read_after_writes: true
   end
 
