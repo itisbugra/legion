4c81f67c4e3ad4b493c4acb01e8b7b9ac62b377b
Buğra Ekuklu <ekuklu@icloud.com>
2018-01-09 18:06:10 +0300

    Added IP/GPS-based locations.

--- apps/legion/lib/legion/identity/auth/concrete/activity.ex
+++ apps/legion/lib/legion/identity/auth/concrete/activity.ex
@@ -8,7 +8,8 @@ defmodule Legion.Identity.Auth.Concrete.Activity do
   alias UAInspector, as: UserAgentParser
   alias FreeGeoIP.Search, as: IPReverseGeocoding
 
-  @user_agent_len 400
+  @env Application.get_env(:legion, Legion.Identity.Auth.Concrete)
+  @user_agent_len Keyword.fetch!(@env, :user_agent_length)
 
   schema "passphrase_activities" do
     belongs_to :passphrase, Passphrase
@@ -27,23 +28,26 @@ defmodule Legion.Identity.Auth.Concrete.Activity do
     field :ip_addr, Legion.Types.INET
     field :country_name, :string
     field :country_code, :string
-    field :location, Legion.Types.Point
+    field :ip_location, Legion.Types.Point
     field :metro_code, :integer
     field :region_code, :string
     field :region_name, :string
     field :time_zone, :string
     field :zip_code, :string
+    field :gps_location, Legion.Types.Point
     field :inserted_at, :naive_datetime, read_after_writes: true
   end
 
+  @doc """
+  Creates a changeset with given parameters.
+  """
+  @spec changeset(Activity, map) :: Ecto.Changeset.t
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:passphrase_id, :ip_addr, :country, :location,
-                     :metro_code, :region_code, :region_name, :time_zone,
-                     :zip_code, :user_agent])
-    |> validate_required([:passphrase_id, :ip_addr, :country, :location,
-                          :metro_code, :region_code, :region_name, :time_zone,
-                          :zip_code, :user_agent])
+    |> cast(params, [:passphrase_id, :ip_addr, :country_name, :country_code,
+                     :ip_location, :metro_code, :region_code, :region_name, :time_zone,
+                     :zip_code, :user_agent, :gps_location])
+    |> validate_required([:passphrase_id, :user_agent, :ip_addr])
     |> validate_length(:user_agent, max: @user_agent_len)
     |> foreign_key_constraint(:passphrase_id)
   end
@@ -51,10 +55,10 @@ defmodule Legion.Identity.Auth.Concrete.Activity do
   @doc """
   Creates a changeset with given passphrase, user agent string and IP address.
   """
-  @spec create_changeset(Passphrase, binary, :inet.ip_address) ::
+  @spec create_changeset(Passphrase, binary, :inet.ip_address, Postgrex.Point.t) ::
     {:ok, Ecto.Changeset.t} |
     {:error, any}
-  def create_changeset(passphrase, user_agent, ip_addr) do
+  def create_changeset(passphrase, user_agent, ip_addr, geo_location) do
     ua_result = UserAgentParser.parse(user_agent)
 
     case IPReverseGeocoding.search(ip_addr) do
@@ -73,19 +77,20 @@ defmodule Legion.Identity.Auth.Concrete.Activity do
             os_name: ua_result.os.name,
             os_platform: ua_result.os.platform,
             os_version: ua_result.os.version,
-            ip_addr: ip_addr,
+            ip_addr: %Postgrex.INET{address: ip_addr},
             country_name: result["country_name"],
             country_code: result["country_code"],
-            location: {result["latitude"], result["longitude"]},
+            ip_location: %Postgrex.Point{x: result["longitude"], y: result["latitude"]},
             metro_code: result["metro_code"],
             region_code: result["region_code"],
             region_name: result["region_name"],
             time_zone: result["time_zone"],
-            zip_code: result["zip_code"]}
+            zip_code: result["zip_code"],
+            geo_location: geo_location}
 
         {:ok, changeset(%__MODULE__{}, params)}
-      {:error, description} ->
-        {:error, description}
+      {:error, %{reason: reason}} ->
+        {:error, reason}
     end
   end
 end
