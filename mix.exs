a932ee96bfd886378639f4c78ce188a3539a5063
Buğra Ekuklu <ekuklu@icloud.com>
2018-01-09 18:08:14 +0300

    Added aliases and coverage settings.

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Legion.Umbrella.Mixfile do
       apps_path: "apps",
       start_permanent: Mix.env == :prod,
       deps: deps(),
+      aliases: aliases(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: ["coveralls": :test, 
                           "coveralls.detail": :test, 
@@ -29,5 +30,13 @@ defmodule Legion.Umbrella.Mixfile do
   defp deps do
     [{:ex_doc, "~> 0.16", only: :dev, runtime: false},
      {:excoveralls, "~> 0.8.0", only: :test}]
+  end
+
+  defp aliases do
+    ["ecto.setup": ["ecto.create",
+                    "ecto.migrate",
+                    "run apps/legion/priv/repo/seeds.exs"],
+     "ecto.reset": ["ecto.drop",
+                    "ecto.setup"],]
   end
 end
