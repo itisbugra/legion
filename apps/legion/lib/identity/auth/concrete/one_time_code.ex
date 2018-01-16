0e2b5e8b82b54da864fd7d3efc3260c28f89b27b
Buğra Ekuklu <ekuklu@icloud.com>
2018-01-16 11:13:57 +0300

    Implementation of OTC.

---
+++ apps/legion/lib/identity/auth/concrete/one_time_code.ex
@@ -0,0 +1,140 @@
+defmodule Legion.Identity.Auth.Concrete.OneTimeCode do
+  @moduledoc """
+  One time codes allow users to enable two-factor authentication (2FA). One may decide to use
+  OTCs with SMS or e-mail. OTC is server-generated, and it cannot be resembled by any client.
+  """
+  use Legion.Stereotype, :model
+
+  @typedoc """
+  Type of a one-time-code.
+  """
+  @type format() :: :integer | :alphanumeric
+
+  @typedoc """
+  """
+  @type algorithm() :: :argon2 | :bcrypt | :pbkdf2
+
+  @typedoc """
+  Represents resulting artifact of generator functions.
+  """
+  @type t() :: binary()
+
+  @env Application.get_env(:legion, Legion.Identity.Auth.OTC)
+  @prefix Keyword.fetch!(@env, :prefix)
+  @postfix Keyword.fetch!(@env, :postfix)
+  @length Keyword.fetch!(@env, :length)
+  @default_type Keyword.fetch!(@env, :type)
+  @digestion Keyword.fetch!(@env, :digestion)
+  @base 10
+  @upper_bound round(:math.pow(@base, @length))
+  @lower_bound round(:math.pow(@base, @length - 1) + 1)
+  @rand_bytes_take_integer 8
+  @rand_bytes_take_alphanumeric 32
+
+  @doc """
+  Generates a one-time-code in given type, 
+  """
+  @spec bingenerate(format()) :: binary()
+  def bingenerate(type) when is_atom(type), do:
+    type |> generate() |> Integer.to_string()
+
+  @doc """
+  Generates a one-time-code in given type.
+  """
+  @spec generate(format()) :: t()
+  def generate(:integer) do
+    @rand_bytes_take_integer
+    |> :crypto.strong_rand_bytes()
+    |> Base.encode16()
+    |> Integer.parse(16)
+    |> Kernel.elem(0)
+    |> Kernel.rem(@upper_bound - @lower_bound)
+    |> Kernel.+(@lower_bound)
+    |> Integer.to_string()
+    |> wrap()
+  end
+  def generate(:alphanumeric) do
+    @rand_bytes_take_alphanumeric
+    |> :crypto.strong_rand_bytes()
+    |> Base.encode32(padding: false)
+    |> String.slice(0..(@length - 1))
+  end
+
+  
+  @doc """
+  Generates a one-time-code with type declared in configuration. Use `generate/1` to explicitly
+  set the type instead.
+  """
+  @spec generate() :: t()
+  def generate(), do: generate(@default_type)
+
+  @doc """
+  Hashes given one-time-code with the digestion algorithm declared in configuration file.
+  """
+  @spec hashpwsalt(t()) :: binary()
+  def hashpwsalt(otc) do
+    otc
+    |> parse()
+    |> hashpwsalt(@digestion)
+  end
+
+  @doc """
+  Hashes given one-time-code with given algorithm.
+
+  Use `hashpwsalt/1` convenience function to rely on configuration.
+  """
+  @spec hashpwsalt(t(), algorithm()) :: binary()
+  def hashpwsalt(otc, :bcrypt), do: Comeonin.Bcrypt.hashpwsalt(otc)
+  def hashpwsalt(otc, :argon2), do: Comeonin.Argon2.hashpwsalt(otc)
+  def hashpwsalt(otc, :pbkdf2), do: Comeonin.Pbkdf2.hashpwsalt(otc)
+
+  @doc """
+  Performs a password check on given one-time-code with corresponding hash, returns truthy value
+  if check was successful.
+  """
+  @spec checkpw(binary(), binary()) :: boolean()
+  def checkpw(otc, hash) when is_binary(otc) and is_binary(hash) do
+    otc
+    |> parse()
+    |> checkpw(hash, @digestion)
+  end
+
+  @doc """
+  Performs a password check on given one-time-code with given hash checking function, returns 
+  truthy value.
+
+  Use `checkpw/0` convenience function to rely on configuration.
+  """
+  @spec checkpw(t(), binary(), algorithm()) :: boolean()
+  def checkpw(otc, hash, :bcrypt), do: Comeonin.Bcrypt.checkpw(otc, hash)
+  def checkpw(otc, hash, :argon2), do: Comeonin.Argon2.checkpw(otc, hash)
+  def checkpw(otc, hash, :pbkdf2), do: Comeonin.Pbkdf2.checkpw(otc, hash)
+
+  @doc """
+  A dummy password check function in order to prevent from user enumeration.
+
+  See documentations for `Comeonin.Bcrypt.dummy_checkpw/2`, `Comeonin.Argon2.dummy_checkpw/2` and
+  `Comeonin.Pbkdf2.dummy_checkpw/2` functions for *Bcrypt*, *Argon2* and *Pbkdf2*, respectively.
+  """
+  @spec dummy_checkpw() :: false
+  def dummy_checkpw(), do: dummy_checkpw(@digestion)
+
+  @doc """
+  Same as `dummy_checkpw/0`, but uses the algorithm supplied as a parameter.
+
+  Use `dummy_checkpw/0` convenience function to rely on configuration.
+  """
+  @spec dummy_checkpw(algorithm()) :: false
+  def dummy_checkpw(:bcrypt), do: Comeonin.Bcrypt.dummy_checkpw()
+  def dummy_checkpw(:argon2), do: Comeonin.Argon2.dummy_checkpw()
+  def dummy_checkpw(:pbkdf2), do: Comeonin.Pbkdf2.dummy_checkpw()
+
+  defp parse(otc) do
+    lower_bound = String.length(@prefix)
+    upper_bound = String.length(otc) - String.length(@postfix)
+
+    String.slice(otc, lower_bound..upper_bound)
+  end
+
+  defp wrap(result), do: "#{@prefix}#{result}#{@postfix}"
+end
---
+++ apps/legion/test/identity/auth/concrete/one_time_code_test.exs
@@ -0,0 +1,114 @@
+defmodule Legion.Identity.Auth.Concrete.OneTimeCodeTest do
+  @moduledoc false
+  use Legion.DataCase
+
+  import Legion.Identity.Auth.Concrete.OneTimeCode
+
+  @env Application.get_env(:legion, Legion.Identity.Auth.OTC)
+  @prefix Keyword.fetch!(@env, :prefix)
+  @postfix Keyword.fetch!(@env, :postfix)
+  @length Keyword.fetch!(@env, :length)
+
+  describe "generate/1" do
+    test "generates an integer one time code" do
+      val = generate(:integer)
+
+      assert String.length(val) == String.length(@prefix) + String.length(@postfix) + @length
+      assert val =~ Regex.compile!("#{@prefix}[0-9]{#{@length}}#{@postfix}")
+    end
+
+    test "generates an alphanumeric one time code" do
+      val = generate(:alphanumeric)
+
+      assert String.length(val) == @length
+    end
+  end
+
+  describe "hashpwsalt/1" do
+    test "hashes given otc" do
+      assert hashpwsalt(generate(:integer))
+    end
+  end
+
+  describe "hashpwsalt/2" do
+    test "hashes given otc with bcrypt" do
+      assert hashpwsalt(generate(:integer), :bcrypt) =~ ~r(\$2b\$12\$)
+    end
+
+    test "hashes given otc with argon2" do
+      assert hashpwsalt(generate(:integer), :argon2) =~ ~r(\$argon2i\$)
+    end
+
+    test "hashes given otc with pbkdf2" do
+      assert hashpwsalt(generate(:integer), :pbkdf2) =~ ~r(\$pbkdf2-sha512\$)
+    end
+  end
+
+  describe "checkpw/2" do
+    test "validates given hash" do
+      otc = generate(:integer)
+      hash = hashpwsalt(otc)
+      check = checkpw(otc, hash)
+
+      assert otc
+      assert hash
+      refute otc == hash
+      assert check
+    end
+  end
+
+  describe "checkpw/3" do
+    test "validates given hash with bcrypt" do
+      otc = generate(:integer)
+      hash = hashpwsalt(otc, :bcrypt)
+      check = checkpw(otc, hash, :bcrypt)
+
+      assert otc
+      assert hash
+      refute otc == hash
+      assert check
+    end
+
+    test "validates given hash pbkdf2" do
+      otc = generate(:integer)
+      hash = hashpwsalt(otc, :pbkdf2)
+      check = checkpw(otc, hash, :pbkdf2)
+
+      assert otc
+      assert hash
+      refute otc == hash
+      assert check
+    end
+
+    test "validates given hash argon2" do
+      otc = generate(:integer)
+      hash = hashpwsalt(otc, :argon2)
+      check = checkpw(otc, hash, :argon2)
+
+      assert otc
+      assert hash
+      refute otc == hash
+      assert check
+    end
+  end
+
+  describe "dummy_checkpw/0" do
+    test "sleeps for a while" do
+      dummy_checkpw()
+    end
+  end
+
+  describe "dummy_checkpw/1" do
+    test "sleeps for a while for argon2" do
+      dummy_checkpw(:argon2)
+    end
+
+    test "sleeps for a while for bcrypt" do
+      dummy_checkpw(:bcrypt)
+    end
+
+    test "sleeps for a while for pbkdf2" do
+      dummy_checkpw(:pbkdf2)
+    end
+  end
+end
