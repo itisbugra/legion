01d8129d87676e3e0f0c66b24e425c4c276b4325
Buğra Ekuklu <ekuklu@icloud.com>
2018-02-01 17:13:17 +0300

    Added moduledoc for the application.

--- apps/legion_web/lib/legion_web/application.ex
+++ apps/legion_web/lib/legion_web/application.ex
@@ -1,4 +1,10 @@
 defmodule LegionWeb.Application do
+  @moduledoc """
+  The HTTP web server interface application service.
+
+  It runs the web interface working with JSON endpoints with the business logic managed by main
+  application.
+  """
   use Application
 
   def start(_type, _args) do
