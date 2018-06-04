08649d972b574c27b3c33a0cc22dd8eda32ed2d2
Buğra Ekuklu <ekuklu@icloud.com>
2018-06-04 12:29:13 +0300

    Removed regression test.

--- apps/legion/test/messaging/switching/globals_test.exs
+++ apps/legion/test/messaging/switching/globals_test.exs
@@ -105,12 +105,6 @@ defmodule Legion.Messaging.Switching.GlobalsTest do
   end
 
   describe "cancel_redirection_for_medium/2" do
-    test "cancels existing redirection", %{user: user} do
-      redirect_medium(user, :apm, :push)
-
-      assert cancel_redirection_for_medium(user, :apm) == :ok
-    end
-
     test "returns error if there was no redirection", %{user: user} do
       assert cancel_redirection_for_medium(user, :apm) == {:error, :no_entry}
     end
