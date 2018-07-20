1357262570716c8eef6460174228c9ab9100d5e8
Buğra Ekuklu <bekuklu@stcpay.com.sa>
2018-07-20 15:18:01 +0300

    Unit tests for concrete authentication.

--- apps/legion/test/identity/auth/auth_test.exs
+++ apps/legion/test/identity/auth/auth_test.exs
@@ -3,15 +3,20 @@ defmodule Legion.Identity.AuthTest do
   use Legion.DataCase
 
   import Legion.Identity.Auth
+  import NaiveDateTime, only: [utc_now: 0, add: 3]
 
   alias Legion.Identity.Auth.Algorithm.Keccak
   alias Legion.Identity.Auth.Insecure.Pair
 
-  @env Application.get_env(:legion, Legion.Identity.Auth.Insecure)
-  @username_length Keyword.fetch!(@env, :username_length)
-  @password_length Keyword.fetch!(@env, :password_length)
+  @insecure_env Application.get_env(:legion, Legion.Identity.Auth.Insecure)
+  @username_length Keyword.fetch!(@insecure_env, :username_length)
+  @password_length Keyword.fetch!(@insecure_env, :password_length)
 
-  describe "registers_internal_user" do
+  @concrete_env Application.get_env(:legion, Legion.Identity.Auth.Concrete)
+  @maximum_allowed_passphrases Keyword.fetch!(@concrete_env, :maximum_allowed_passphrases)
+  @passphrase_lifetime Keyword.fetch!(@concrete_env, :passphrase_lifetime)
+
+  describe "registers_internal_user/2" do
     setup do
       %{username: random_string(Enum.min(@username_length)), 
         password: random_string(@password_length)}
@@ -48,7 +53,7 @@ defmodule Legion.Identity.AuthTest do
     end
   end
 
-  describe "generate_passphrase" do
+  describe "generate_passphrase/4" do
     setup do
       user = insert(:user)
       password = random_string(@password_length)
@@ -63,10 +68,44 @@ defmodule Legion.Identity.AuthTest do
         ip_addr: {1, 1, 1, 1}}
     end
 
-    # test "generates passphrase", %{password_hash: password_hash, pair: pair, ip_addr: ip_addr} do
-    #   result = generate_passphrase(pair.username, password_hash, ip_addr)
+    test "matching username/password combination returns passkey", %{password_hash: password_hash, pair: pair, ip_addr: ip_addr} do
+      assert match? {:ok, :require, _}, generate_passphrase(pair.username, password_hash, ip_addr)
+    end
 
-    #   assert Kernel.elem(result, 0) == :ok
-    # end
+    test "if user does not exist says no user", %{password_hash: password_hash, ip_addr: ip_addr} do
+      assert {:error, :no_user_verify} == generate_passphrase("_failing_some_username", password_hash, ip_addr)
+    end
+
+    test "returns wrong password if hash is not verified", %{pair: pair, password_hash: password_hash, ip_addr: ip_addr} do
+      assert {:error, :wrong_password} == generate_passphrase(pair.username, random_string(String.length(password_hash)), ip_addr)
+    end
+
+    test "errors if user already has maximum amount of passphrases exceed", %{pair: pair, password_hash: password_hash, ip_addr: ip_addr} do
+      _passphrases = insert_list(@maximum_allowed_passphrases, :passphrase, user: pair.user)
+
+      assert {:error, :maximum_passphrases_exceeded} == generate_passphrase(pair.username, password_hash, ip_addr)
+    end
+
+    @tag :regression
+    test "outdated passphrases do not count in allowance limit", %{pair: pair, password_hash: password_hash, ip_addr: ip_addr} do
+      invalidation_moment = add(utc_now(), -(@passphrase_lifetime + 1), :second)
+
+      _passphrases = insert_list(@maximum_allowed_passphrases, :passphrase, user: pair.user, inserted_at: invalidation_moment)
+
+      assert match? {:ok, :require, _}, generate_passphrase(pair.username, password_hash, ip_addr)
+    end
+
+    @tag :regression
+    test "invalidated passphrases do not count in allowance limit", %{pair: pair, password_hash: password_hash, ip_addr: ip_addr} do
+      source = insert(:passphrase, user: pair.user)
+
+      @maximum_allowed_passphrases
+      |> insert_list(:passphrase, user: pair.user)
+      |> Enum.each(fn target ->
+        insert(:passphrase_invalidation, source_passphrase: source, target_passphrase: target)
+      end)
+
+      assert match? {:ok, :require, _}, generate_passphrase(pair.username, password_hash, ip_addr)
+    end
   end
 end
