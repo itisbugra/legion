ff019c5e662ebff77edbed3642dd599a3f444088
Buğra Ekuklu <bekuklu@stcpay.com.sa>
2018-11-07 18:40:48 +0300

    View model for valid safety traits.

---
+++ apps/legion/lib/identity/telephony/phone_number/valid_safety_trait.ex
@@ -0,0 +1,31 @@
+defmodule Legion.Identity.Telephony.PhoneNumber.ValidSafetyTrait do
+  @moduledoc """
+  A view for the safety traits of phone numbers filtering only valid ones.
+  """
+  use Legion.Stereotype, :viewdecl
+
+  alias Legion.Identity.Telephony.PhoneNumber
+  alias Legion.Identity.Auth.Concrete.Passphrase
+
+  create do
+    """
+    CREATE OR REPLACE VIEW user_phone_number_valid_safety_traits AS
+      SELECT *
+      FROM user_phone_number_safety_traits upnst
+      WHERE (upnst.inserted_at + upnst.valid_for * '1 second'::interval) > now();
+    """
+  end
+
+  drop do
+    """
+    DROP VIEW user_phone_number_valid_safety_traits;
+    """
+  end
+
+  schema "user_phone_number_valid_safety_traits" do
+    belongs_to :phone_number, PhoneNumber
+    belongs_to :authority, Passphrase
+    field :valid_for, :integer
+    field :inserted_at, :naive_datetime
+  end
+end
