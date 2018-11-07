4667461e3673d7f9c537d72b023e09b645b72a4e
Buğra Ekuklu <bekuklu@stcpay.com.sa>
2018-11-07 18:32:27 +0300

    Stubs to reuse before test factory setups.

--- apps/legion/mix.exs
+++ apps/legion/mix.exs
@@ -29,7 +29,7 @@ defmodule Legion.Mixfile do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:test), do: ["lib", "test/support", "test/stubs"]
   defp elixirc_paths(_),     do: ["lib"]
 
   # Specifies your project dependencies.
