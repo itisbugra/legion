71c843954fd7f549b07fa3d1e7cab668cb180d0f
Buğra Ekuklu <bekuklu@stcpay.com.sa>
2018-11-07 18:43:07 +0300

    Unit tests for neglection traits.

---
+++ apps/legion/test/identity/telephony/phone_number/neglection_trait_test.exs
@@ -0,0 +1,42 @@
+defmodule Legion.Identity.Telephony.PhoneNumber.NeglectionTraitTest do
+  @moduledoc false
+  use Legion.DataCase
+
+  alias Legion.Identity.Telephony.PhoneNumber.NeglectionTrait
+
+  @valid_params %{phone_number_id: 1,
+                  authority_id: 1}
+
+  test "changeset with valid attrs" do
+    changeset = 
+      NeglectionTrait.changeset(%NeglectionTrait{}, 
+                                @valid_params)
+
+    assert changeset.valid?
+  end
+
+  test "changeset without phone number identifier" do
+    changeset =
+      NeglectionTrait.changeset(%NeglectionTrait{},
+                                omit_param(:phone_number_id))
+
+    refute changeset.valid?
+  end
+
+  test "changeset without authority passphrase identifier" do
+    changeset =
+      NeglectionTrait.changeset(%NeglectionTrait{},
+                                omit_param(:authority_id))
+
+    refute changeset.valid?
+  end
+
+  test "changeset with default params" do
+    changeset = NeglectionTrait.changeset(%NeglectionTrait{})
+
+    refute changeset.valid?
+  end
+
+  defp omit_param(field), do:
+    Map.delete(@valid_params, field)
+end
