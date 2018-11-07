8bcc2ff7f26df15d780b439f3f05073da34021c9
Buğra Ekuklu <bekuklu@stcpay.com.sa>
2018-11-07 18:40:26 +0300

    Neglection traits for ignoring phone numbers.

---
+++ apps/legion/lib/identity/telephony/phone_number/neglection_trait.ex
@@ -0,0 +1,32 @@
+defmodule Legion.Identity.Telephony.PhoneNumber.NeglectionTrait do
+  @moduledoc """
+  Marks a phone number as neglected upon grant.
+
+  Defines a normalized event sink for neglection actions.
+
+  # Schema fields
+
+  - `-:phone_number`: The phone number being affected from the neglection trait.
+  - `:authority`: The authority who marked the phone number as ignored.
+  - `:inserted_at`: The timestamp of the neglection action, immutable.
+  """
+  use Legion.Stereotype, :model
+
+  alias Legion.Identity.Telephony.PhoneNumber
+  alias Legion.Identity.Auth.Concrete.Passphrase
+
+  schema "user_phone_number_neglection_traits" do
+    belongs_to :phone_number, PhoneNumber
+    belongs_to :authority, Passphrase
+    field :inserted_at, :naive_datetime, read_after_writes: true
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:phone_number_id, :authority_id])
+    |> validate_required([:phone_number_id, :authority_id])
+    |> foreign_key_constraint(:phone_number_id)
+    |> foreign_key_constraint(:authority_id)
+    |> unique_constraint(:phone_number_id)
+  end
+end
