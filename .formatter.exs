e8d9d4ebc0f9e329278eab4c47f819e158e5e0b4
Buğra Ekuklu <bekuklu@stcpay.com.sa>
2018-11-13 02:59:35 +0300

    Ignore Ecto macro magic.

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,23 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    # Ecto magic
+    from: 2,
+    defenum: 2,
+    defenum: 3,
+    belongs_to: 2,
+    belongs_to: 3,
+    has_one: 2,
+    has_one: 3,
+    has_many: 2,
+    has_many: 3,
+    many_to_many: 2,
+    many_to_many: 3,
+    field: 2,
+    field: 3,
+
+    # Registry directory
+    defregdir: 2
+  ]
 ]
