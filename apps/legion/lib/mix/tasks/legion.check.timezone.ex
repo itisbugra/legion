f83ff4056f04866db069b601a40f0d632b82aa53
Buğra Ekuklu <ekuklu@icloud.com>
2021-01-23 06:49:59 +0300

    Support for the IANA timezone on UTC.

--- apps/legion/lib/mix/tasks/legion.check.timezone.ex
+++ apps/legion/lib/mix/tasks/legion.check.timezone.ex
@@ -14,7 +14,7 @@ defmodule Mix.Tasks.Legion.Check.Timezone do
 
       {:ok, %Postgrex.Result{rows: [[tz]]}} = repo.query("SHOW TIME ZONE", [], log: false)
 
-      if tz == "UTC" do
+      if tz =~ ~r/^(Etc\/)?UTC$/ do
         Mix.shell().info("time zone = UTC")
       else
         Mix.raise("the timezone setting of the database is not UTC, but #{tz} instead")
